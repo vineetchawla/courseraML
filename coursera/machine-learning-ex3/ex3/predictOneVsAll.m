a215b998aa371b88785b4fed3f875fdc51f06ba7
Vineet Chawla <vineetchawla19@gmail.com>
2016-06-18 16:15:36 +0200

    predict oneVall

--- coursera/machine-learning-ex3/ex3/predictOneVsAll.m
+++ coursera/machine-learning-ex3/ex3/predictOneVsAll.m
@@ -29,8 +29,10 @@ X = [ones(m, 1) X];
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
 %       
+%h_theta = X * all_theta';
+%p = max(h_theta, [], 2);
 
-
+[val, p] = max(X * all_theta', [], 2);
 
 
 
