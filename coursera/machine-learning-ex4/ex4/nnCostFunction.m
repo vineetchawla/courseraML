41965e56f17285592bc070013b6bcdb44d61f591
Vineet Chawla <vineetchawla19@gmail.com>
2016-06-25 15:37:38 +0200

    adding unregularized cost

--- coursera/machine-learning-ex4/ex4/nnCostFunction.m
+++ coursera/machine-learning-ex4/ex4/nnCostFunction.m
@@ -39,6 +39,22 @@ Theta2_grad = zeros(size(Theta2));
 %         cost function computation is correct by verifying the cost
 %         computed in ex4.m
 %
+
+Y = zeros(m, num_labels);
+I = eye(num_labels);
+
+for i = 1:m
+    Y(i,:) = I(y(i),:);
+end
+
+a1 = [ones(m,1), X];
+a2 = sigmoid(a1 * Theta1');
+
+a2 = [ones(size(a2,1) ,1), a2];
+a3 = sigmoid(a2 * Theta2');
+
+J = (-1/m) * sum((Y.*log(a3) + (1-Y).*log(1-a3)), 2);
+
 % Part 2: Implement the backpropagation algorithm to compute the gradients
 %         Theta1_grad and Theta2_grad. You should return the partial derivatives of
 %         the cost function with respect to Theta1 and Theta2 in Theta1_grad and
@@ -63,7 +79,7 @@ Theta2_grad = zeros(size(Theta2));
 %
 
 
-
+ 
 
 
 
